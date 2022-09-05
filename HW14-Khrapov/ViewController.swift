f2d1aa594d53d72b8a7fc20793936e8c213c75c2
Anton Khrapov <froe054@mail.ru>
2022-09-05 22:21:20 +0700

    Initial Commit

---
+++ HW14-Khrapov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW14-Khrapov
+//
+//  Created by Anton on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
